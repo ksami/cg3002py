678d83cfa9ccebec3d522b4fad1757d579eb1afd
ksami <ksami.ihide@gmail.com>
2014-09-17 14:47:46 +0800

    python process calling c process, compile cprocess.cpp first

---
+++ cprocess.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int timer = 0;
+	int i;
+
+	cout << "cprocess started" << endl;
+
+	for(i=0; i<100; i++){
+		while(timer<=100000000){
+			timer++;
+		}
+		timer = 0;
+		cout << "cprocess" << i << endl;
+	}
+}
---
+++ cpython.py
@@ -0,0 +1,25 @@
+# Python process to call C process
+
+import subprocess
+import sys
+
+def execute(command):
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+	# Poll process for new output until finished
+	while True:
+		nextline = process.stdout.readline()
+		if nextline == '' and process.poll() != None:
+			break
+			
+		#print nextline
+		sys.stdout.write(nextline)
+		sys.stdout.flush()
+
+	output = process.communicate()[0]
+	exitCode = process.returncode
+
+# test with cpython cprocess.o
+if __name__ == '__main__':
+	print 'executing {program}'.format(program=sys.argv[1])
+	execute(sys.argv[1])
