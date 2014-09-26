211a4c456900b7e9c80573d29c1111eaa7a2e551
ksami <ksami.ihide@gmail.com>
2014-09-26 15:47:32 +0800

    add a test file to pedometer to add into scheduling

--- mult.py
+++ mult.py
@@ -7,6 +7,7 @@ import timer
 import smwmap
 import cpython
 import audio.textspeech
+import pedometer.test
 
 cameraExe = "./cprocess.o"
 mapName = "COM1"
@@ -16,12 +17,14 @@ if __name__ == "__main__":
 	q_cam = multiprocessing.Queue()
 	q_map = multiprocessing.Queue()
 
+	pedo = multiprocessing.Process(target=pedometer.test.execute)
 	camera = multiprocessing.Process(target=cpython.execute, args=(q_cam, cameraExe))
 	texttospeech = multiprocessing.Process(target=audio.textspeech.speakq, args=(q_cam,))
 	alarm = multiprocessing.Process(target=timer.alarm, args=(4,))
 	getmap = multiprocessing.Process(target=smwmap.obtainMap, args=(q_map, mapName, mapFloor))
 
 	# start processes
+	pedo.start()
 	camera.start()
 	texttospeech.start()
 	alarm.start()
@@ -37,6 +40,7 @@ if __name__ == "__main__":
 	print repr(mapinfo)
 
 	# wait for processes to end
+	pedo.join()
 	camera.join()
 	texttospeech.join()
 	alarm.join()
---
+++ pedometer/test.py
@@ -0,0 +1,6 @@
+from accelerometer import Accelerometer
+import time
+
+def execute():
+	time.sleep(7)
+	print 'pedometer test'
