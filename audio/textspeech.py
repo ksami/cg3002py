0c81f86c9d0a95188ed2fb27edf5527bcc64f5a1
codeihsan <ihsanrowther13@gmail.com>
2014-10-18 10:14:38 +0800

    Update textspeech.py
    Ken is this the style u wan?

--- audio/textspeech.py
+++ audio/textspeech.py
@@ -1,15 +1,27 @@
 import sys,os,time
 
-def speakq(q_string):
-	while True:
-		myString = q_string.get()
-		print myString
-		command = 'espeak -s 155 "'+myString+'" > /dev/null 2>&1'
+class Read(object):
+	#Commands to read out
+	cmd_list = {'sp': 'State starting position', 'e': 'State your ending position', 'c': 'Did you say' 'gf': 'Go Forward', 'tl': 'Turn Left', 'tr': 'Turn Right'}
+	
+	def sysinit(sys_queue):
+		if(sys_queue.get == 'sp'):
+			command = 'espeak -s 155 "'+cmd_list['sp']+'" > dev/null 2>&1'
+			os.system(command)
+		
+	
+	def speakq(q_string):
+		while True:
+			myString = q_string.get()
+			print myString
+			command = 'espeak -s 155 "'+myString+'" > /dev/null 2>&1'
+			os.system(command)
+
+	def speak(myString):
+		command = 'espeak -s 155 "'+myString+'"'
 		os.system(command)
 
-def speak(myString):
-	command = 'espeak -s 155 "'+myString+'"'
-	os.system(command)
+
 
 # for testing
 # call with python textspeech.py texttoreadout
