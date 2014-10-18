acc13a2dc9e2c404ba93ac52ec484104dc8937f1
codeihsan <ihsanrowther13@gmail.com>
2014-10-18 11:32:57 +0800

    Update textspeech.py
    Haven tested. Hope you are not planning on killing the process. Then the code won't work,

--- audio/textspeech.py
+++ audio/textspeech.py
@@ -1,23 +1,41 @@
 import sys,os,time
 
-class Read(object):
+class Speak(object):
 	#Commands to read out
-	cmd_list = {'sp': 'State starting position', 'e': 'State your ending position', 'c': 'Did you say' 'gf': 'Go Forward', 'tl': 'Turn Left', 'tr': 'Turn Right'}
+	cmd_list = {'sp': 'State starting position', 'e': 'State your ending position', 'c': 'Did you say ' 'gf': 'Go Forward', 'tl': 'Turn Left', 'tr': 'Turn Right'}
 	
+	def __init__(self):
+		self.state = 0
+		self.prevcmd = 'tl'
+	#need to think of a better way to write this
 	def sysinit(sys_queue):
-		if(sys_queue.get == 'sp'):
-			command = 'espeak -s 155 "'+cmd_list['sp']+'" > dev/null 2>&1'
+		if(sys_queue.get() == 'c'):
+			command = 'espeak -s 155 "'+cmd_list['c']+sys_queue.get() +'" > dev/null 2>&1'
 			os.system(command)
+			self.state = self.state -1
+			
+		else:
+			if((sys_queue.get() == 'sp') and (self.state == 0)):
+				command = 'espeak -s 155 "'+cmd_list['sp']+'" > dev/null 2>&1'
+				os.system(command)
+				self.state = self.state +1
+				
+			if(sys_queue.get() == 'ep' and (self.state == 1)):
+				command = 'espeak -s 155 "'+cmd_list['ep']+'" > dev/null 2>&1'
+				os.system(command)
+				self.state = self.state +1
+				
 		
-	
-	def speakq(q_string):
-		while True:
-			myString = q_string.get()
-			print myString
-			command = 'espeak -s 155 "'+myString+'" > /dev/null 2>&1'
+	def giveCmd(sys_queue):
+		if(self.prevcmd == 'gf' and sys_queue.get() == 'gf'):
+			#Do nothing
+		else:
+			self.prevcmd = sys_queue.get()
+			command = 'espeak -s 155 "'+self.prevcmd+'" > /dev/null 2>&1'
 			os.system(command)
-
-	def speak(myString):
+	
+	#just for testing
+	def speakTest(myString):
 		command = 'espeak -s 155 "'+myString+'"'
 		os.system(command)
 
