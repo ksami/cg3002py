b0b8a2e159e72c60df02edec8d17505ab20c7050
ksami <ksami.ihide@gmail.com>
2014-10-22 19:08:08 +0800

    testing kill child process

--- audio/speechtext.py
+++ audio/speechtext.py
@@ -1,14 +1,22 @@
-import sys,os
+import sys,os,signal
 import subprocess
 
 class Listen:
 	program = "exec /home/pi/cg3002py/audio/cont -samprate 48000 -dict /home/pi/pocketsphinx-0.8/model/lm/en_US/cmu07a.dic -nfft 2048"
 
+	def __init__(self):
+		self.pid = 0
+
+	def signal_term_handler(self, signum, frame):
+		os.killpg(self.pid, signal.SIGTERM)
+
 	#params:
 	#q_listen: output from pocketsphinx
 	def listen(self, q_listen):
-		process = subprocess.Popen(Listen.program, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		process = subprocess.Popen(Listen.program, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, preexec_fn=os.setsid)
+		self.pid = process.pid
 		print "listen running"
+		signal.signal(signal.SIGTERM, signal_term_handler)
 		# Poll process for new output until finished
 		while process.poll() == None:
 			# read output
