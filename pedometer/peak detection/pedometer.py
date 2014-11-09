7200eb7db2e8aabca91af89bc8a8961ecd9f61ee
ksami@pi <ksami.ihide@gmail.com>
2014-11-09 20:32:21 +0800

    calibration for soft walking

--- pedometer/peak detection/pedometer.py
+++ pedometer/peak detection/pedometer.py
@@ -8,8 +8,8 @@ ACCEL_THRESHOLD = 1000 # = 32767 - 31128 (max and min values when stabilized)
 PEAK_THRESHOLD = 5000
 TIME_THRESHOLD = 0.4
 HIGH_PASS = 0.8
-#STRIDE_COEFFICIENT = 
-STRIDE_COEFFICIENT = 1.319148936
+STRIDE_COEFFICIENT = 1
+#STRIDE_COEFFICIENT = 1.319148936
 # from walking in total of 14.4 meters and total_distance (without calibration) 545.260574691
 HEIGHT = 1.76
 
@@ -171,7 +171,7 @@ accel_offset_z = 0
 # print "\nCALIBRATION SIZE: " , calibration_size  
 # print "OFFSET: ", accel_offset_x, accel_offset_y, accel_offset_z
 
-accel_graph = open('accel_graph_teehee.txt', 'w')
+#accel_graph = open('accel_graph_teehee.txt', 'w')
 print "STRIDE_COEFFICIENT: ", STRIDE_COEFFICIENT
 print "START!!"
 
@@ -181,7 +181,7 @@ first_time = True
 
 
 time_elapsed = time.time()
-while(total_distance <= 2150):
+while(True):
 
     size += 1
 
@@ -206,7 +206,7 @@ while(total_distance <= 2150):
 
     accel_filter_list.insert(moving_index, accel_val)
     moving_index = (moving_index + 1) % 4
-    accel_graph.write(str(size) + "\t" + str(accel_val.x) + "\t" + str(accel_val.y) + "\t" + str(accel_val.z))
+    #accel_graph.write(str(size) + "\t" + str(accel_val.x) + "\t" + str(accel_val.y) + "\t" + str(accel_val.z))
     
     # finding minima and maxima
     if(not first_time):
@@ -229,7 +229,7 @@ while(total_distance <= 2150):
                         print "stride length", stride_length
                         print "total distance:", total_distance
                         accel_list = []
-                        accel_graph.write("\t" + "maxima teehee")
+                        #accel_graph.write("\t" + "maxima teehee")
                         sum_diff += math.fabs(accel_maxima.y - accel_minima.y)
                         num_diff += 1
                         calculate_distance = False
@@ -242,7 +242,7 @@ while(total_distance <= 2150):
                             peak_direction = MAXIMA
                             accel_minima = accel_val
                             time_window = time.time()
-                            print "PEAK DETECTED MINIMA", num_steps
+                            print "------ PEAK DETECTED MINIMA", num_steps
                             peak_threshold = PEAK_THRESHOLD
                             calculate_distance = True
 
@@ -260,7 +260,7 @@ while(total_distance <= 2150):
                         total_distance += stride_length
                         print "stride length", stride_length
                         print "total distance:", total_distance
-                        accel_graph.write("\t" + "minima teehee")
+                        #accel_graph.write("\t" + "minima teehee")
                         sum_diff += math.fabs(accel_maxima.y - accel_minima.y)
                         num_diff += 1
                         accel_list = []
@@ -274,7 +274,7 @@ while(total_distance <= 2150):
                             peak_direction = MINIMA
                             accel_maxima = accel_val
                             time_window = time.time()
-                            print "PEAK DETECTED MAXIMA", num_steps
+                            print "----- PEAK DETECTED MAXIMA", num_steps
                             peak_threshold = PEAK_THRESHOLD
                             calculate_distance = True
 
@@ -282,12 +282,12 @@ while(total_distance <= 2150):
         peak_direction = MINIMA
         peak_threshold = PEAK_THRESHOLD / 2
         accel_maxima = accel_val
-        accel_graph.write("\t" + "maxima teehee")
+        #accel_graph.write("\t" + "maxima teehee")
         first_time = False
         sample_new = accel_val
         time_window = time.time()
 
-    accel_graph.write("\n")
+    #accel_graph.write("\n")
 
 print "threshold ", sum_diff / num_diff
-accel_graph.close()
+#accel_graph.close()
