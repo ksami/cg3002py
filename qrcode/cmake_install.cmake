59b0a33ff6eb6404b4d00301d95217bac51dad17
ksami@pi <ksami.ihide@gmail.com>
2014-11-09 16:56:49 +0800

    build files

--- qrcode/cmake_install.cmake
+++ qrcode/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/pi/opencv/qrcode
+# Install script for directory: /home/pi/cg3002py/qrcode
 
 # Set the install prefix
 IF(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -38,7 +38,7 @@ ELSE(CMAKE_INSTALL_COMPONENT)
   SET(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
 ENDIF(CMAKE_INSTALL_COMPONENT)
 
-FILE(WRITE "/home/pi/opencv/qrcode/${CMAKE_INSTALL_MANIFEST}" "")
+FILE(WRITE "/home/pi/cg3002py/qrcode/${CMAKE_INSTALL_MANIFEST}" "")
 FOREACH(file ${CMAKE_INSTALL_MANIFEST_FILES})
-  FILE(APPEND "/home/pi/opencv/qrcode/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
+  FILE(APPEND "/home/pi/cg3002py/qrcode/${CMAKE_INSTALL_MANIFEST}" "${file}\n")
 ENDFOREACH(file)
